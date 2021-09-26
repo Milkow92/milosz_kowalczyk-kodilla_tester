40c308606658e19b945ef795e9d8af3b1cb76175
Milosz kowalczyk <mily92@tlen.pl>
2021-09-26 14:26:59 +0200

    initial version

---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,11 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Milosz";
+        int myAge = 29;
+        double height = 1.80;
+        char carClass = 'C';
+        boolean isGoodProgrammer = false;
+        int howManyCars = 1;
+        System.out.println(myName+'\n'+myAge+'\n'+height+'\n'+carClass+'\n'+isGoodProgrammer+'\n' +howManyCars);
+    }
+}
