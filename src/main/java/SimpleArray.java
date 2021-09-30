c7c5f084ef0d001ac7b6deb881bd3318f9979fe7
Milosz kowalczyk <mily92@tlen.pl>
2021-09-30 19:39:42 +0200

    initlia version

---
+++ src/main/java/SimpleArray.java
@@ -0,0 +1,12 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+
+        String[] movies = new String[5];
+        movies[0] = "Harry Potter";
+        movies[1] = "Lord of the Rings";
+        movies[2] = "Fast adn Furious";
+        movies[3] = "I am Legend";
+        movies[4] = "Star Wars";
+        int numberOfElements = movies.length;
+    }
+}
