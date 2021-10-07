441b906001d34410df0042af2cbdd153bf06a796
Milosz kowalczyk <mily92@tlen.pl>
2021-10-07 18:48:35 +0200

    initialVersion

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -1,5 +1,9 @@
 public class LeapYear {
-    public static void main (String[] args){
-
+public boolean checkIfLeapYear(int year){
+    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+}
+    public static void main (String[] args) {
+        LeapYear object = new LeapYear();
+    System.out.println(object.checkIfLeapYear(1916));
     }
 }
