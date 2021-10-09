44600ee230f13be3f86c300a5b4f3c48844f8bd0
Milosz kowalczyk <mily92@tlen.pl>
2021-10-09 11:19:10 +0200

    Initial Version

--- src/main/java/Loops.java
+++ src/main/java/Loops.java
@@ -1,9 +1,10 @@
 public class Loops {
     public static void main(String[] args) {
 
+        String[] names = new String[] {"Zygfryd", "Gwidon", "Florentyna"};
 
-        for (int i = 0; i <= 10; i++) {
-            System.out.println(i);
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
         }
     }
 }
